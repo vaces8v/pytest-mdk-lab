343e24e0a1efc15afd6081ecaea29064b2b7f3d2
vaceslavchek667689 <vaceslavchek667689@gmail.com>
2024-09-13 08:45:17 +0000

    init repo
    Создания репозитория

---
+++ main.py
@@ -0,0 +1,17 @@
+def pluse2(nums):
+    result = []
+    for num in nums:
+        result.append(num + 2)
+    return result
+
+def mulpitly2(nums):
+    result = []
+    for num in nums:
+        result.append(num * 2)
+    return result
+
+def exponent2(nums):
+    result = []
+    for num in nums:
+        result.append(num ** 2)
+    return result
