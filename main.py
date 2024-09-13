8345ad44e5992b0368908e5d083a7fa919932965
vaceslavchek667689 <vaceslavchek667689@gmail.com>
2024-09-13 08:55:40 +0000

    UwU
    Тест division

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-def pluse2(nums):
-    result = []
-    for num in nums:
-        result.append(num + 2)
-    return result
-
-def mulpitly2(nums):
-    result = []
-    for num in nums:
-        result.append(num * 2)
-    return result
-
-def exponent2(nums):
-    result = []
-    for num in nums:
-        result.append(num ** 2)
-    return result
+def divison(a, b):
+    return a / b
--- test_main.py
+++ test_main.py
@@ -1,17 +1,4 @@
-import pytest
 import main
 
-@pytest.fixture()
-def get_prime_nums():
-    prime_nums = []
-    for num in range(1, 50):
-        for div in range(2, num):
-            if num % div == 0:
-                break
-        else:
-            prime_nums.append(num)
-    return prime_nums
-
-def test_pluse(get_prime_nums):
-    prime_nums = get_prime_nums
-    assert main.pluse2(prime_nums) == [3, 4, 5, 7, 9, 13, 15, 19, 21, 25, 31, 33, 39, 43, 45, 49]
+def test_divison_good():
+    assert main.divison(10, 2) == 5.0
