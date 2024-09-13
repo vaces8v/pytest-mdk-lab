b6b908c203e680c8a2d7ddad15749e16b3c38b2a
vaceslavchek667689 <vaceslavchek667689@gmail.com>
2024-09-13 09:07:40 +0000

    @parametrize divison

--- test_main.py
+++ test_main.py
@@ -1,13 +1,9 @@
 import main
+import pytest
 
-def test_divison_good():
-    assert main.divison(10, 2) == 5.0
-
-def test_divison_2alpha():
-    assert main.divison(10, 5) == 2.0
-
-def test_divison_3beta():
-    assert main.divison(20, 2) == 10.0
-
-def test_divison_4negative():
-    assert main.divison(10,-2) == -5.0
+@pytest.mark.parametrize("a, b, expresult", [(10, 2, 5),
+                                             (20, 10, 2),
+                                             (30, -3, -10),
+                                             (5, 2, 2.5)])
+def test_divison_good(a, b, expresult):
+    assert main.divison(a, b) == expresult
