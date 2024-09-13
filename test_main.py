ac78dd6479ff48cdb4a1e33a86e05924198166a6
vaceslavchek667689 <vaceslavchek667689@gmail.com>
2024-09-13 09:02:18 +0000

    Test for 4 divison
    Тест для 4 division: good, 2alpha, 3beta, 4negative. littel baka susy uwu

--- test_main.py
+++ test_main.py
@@ -2,3 +2,12 @@ import main
 
 def test_divison_good():
     assert main.divison(10, 2) == 5.0
+
+def test_divison_2alpha():
+    assert main.divison(10, 5) == 2.0
+
+def test_divison_3beta():
+    assert main.divison(20, 2) == 10.0
+
+def test_divison_4negative():
+    assert main.divison(10,-2) == -5.0
